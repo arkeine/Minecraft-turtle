b8217fb40c44e00172189fe307f842c348a0da51
Nils Ryter <nils.ryter@he-arc.ch>
2016-02-09 22:37:59 +0100

    Make the launcher load minimal libraries

--- launcher.lua
+++ launcher.lua
@@ -1,1 +1,48 @@
-print("WOOORK")
+-------------------------------------------------
+--	Autor :			Arkeine
+--	Date :			09.02.2016
+--	Purpose :		Lauch my programe easily with loading dependencies
+-------------------------------------------------
+
+Launcher = {}
+Launcher.program = {}
+Launcher.library = {}
+Launcher.MAX_PROGRAM_PAGE = 6
+Launcher.LIBRARIES_DIR = "/arkeine-lib/"
+Launcher.PROGRAMS_DIR = "/arkeine-prg/"
+
+-------------------------------------------------
+--	Load librairies
+-------------------------------------------------
+
+Launcher.listProgram = function()
+	local n=0
+
+	for k,v in pairs(Launcher.program) do
+		n=n+1
+		print(k)
+		if n >= Launcher.MAX_PROGRAM_PAGE then
+			print("Press a key for more...")
+			n = 0
+		end
+	end
+end
+
+
+
+-------------------------------------------------
+--	Init
+-------------------------------------------------
+
+require Loader
+local result = Loader.getGithub("libraries/ConsoleGUI.lua", Launcher.LIBRARIES_DIR.."ConsoleGUI")
+
+if not result then
+	print("Loading fail")
+	os.exit()
+end
+
+require ConsoleGUI
+ConsoleGUI.displaySplashScreen()
+
+
